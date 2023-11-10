1d4fa1979644dc30462b542892066ab4cb612d32
WNCRY <dimacelujkin@gmail.com>
2023-11-10 15:07:30 +0200

    add interface

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
-const Favorite = () => {
+const Favorite: React.FC = () => {
   const { isAuth } = useAuth();
 
   return <div>{!isAuth && <Navigate to={'/'}></Navigate>}</div>;
